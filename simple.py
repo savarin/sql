ae624ebb915cc90f1999a13f9d0d43c2bf9c0f30
Ezzeri Esa <esa@squareup.com>
2018-02-11 16:54:29 -0800

    changed init to read column names

--- simple.py
+++ simple.py
@@ -5,11 +5,19 @@ class FileScan(object):
     def __init__(self, table):
         self.path = {'movies': 'movies.csv', 'ratings': 'ratings.csv'}
         self.file = open(self.path.get(table), 'r')
-        self.stub = ''
-        self.columns = []
-        self.counter = 0
+
+        data = self.file.read(4096).split('\r\n')
+        self.columns = data.pop(0).split(',')
+        self.stub = data
+        self.counter = 0        
 
     def next(self):
+        self.counter += 1
+
+        if self.counter == 1:
+            result, self.stub = self.stub, ''
+            return result
+
         block = self.stub + self.file.read(4096)
 
         if not block:
@@ -19,14 +27,15 @@ class FileScan(object):
         data = block.split('\r\n')
         self.stub = data.pop()
 
-        if not self.counter:
-            self.columns = data.pop(0).split(',')
-
-        self.counter += 1
         return data
 
     def close(self):
         self.file.close()
+
+
+class Selection(object):
+    def selection(column, identifier):
+        index = column
 
 
 if __name__ == '__main__':
