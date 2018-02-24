530552bf039bca2955bf2f8ba18d26d6d2a5a5ce
Ezzeri Esa <esa@squareup.com>
2018-02-23 22:43:30 -0800

    implemented b+ tree with rebranching

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 
 
 class Node(object):
-    def __init__(self, order=2):
+    def __init__(self, order):
         self.order = order
         self.keys = []
         self.values = []
@@ -10,15 +10,22 @@ class Node(object):
     def is_full(self):
         return len(self.keys) == self.order
 
-    def find(self, key):
-        if self.leaf:
-            return self
+    def add(self, key, value):
+        if not self.keys:
+            self.keys.append(key)
+            self.values.append(value)
+            return None
 
         for i, item in enumerate(self.keys):
             if key < item:
-                return self.values[i].find(key)
+                self.keys = self.keys[:i] + [key] + self.keys[i:]
+                self.values = self.values[:i] + [value] + self.values[i:]
+                break
 
-        return self.values[i + 1].find(key)
+            elif i + 1 == len(self.keys):
+                self.keys.append(key)
+                self.values.append(value)
+                break
 
     def split(self):
         left = Node(self.order)
@@ -35,31 +42,55 @@ class Node(object):
         self.values = [left, right]
         self.leaf = False
 
-    def insert(self, key, value):
-        leaf = self.find(key)
-
-        if len(leaf.keys) == 0:
-            leaf.keys.append(key)
-            leaf.values.append(value)
-            return None
-
-        for i, item in enumerate(leaf.keys):
-            if key < item:
-                leaf.keys = leaf.keys[:i] + [key] + leaf.keys[i:]
-                leaf.values = leaf.values[:i] + [value] + leaf.values[i:]
-                break
-
-            elif i + 1 == len(leaf.keys):
-                leaf.keys.append(key)
-                leaf.values.append(value)
-                break
-
-        if leaf.is_full():
-            leaf.split()
-
     def show(self, counter=0):
-        print counter, str(self.keys), self.leaf
+        print counter, str(self.keys)
 
         if not self.leaf:
             for item in self.values:
                 item.show(counter + 1)
+
+
+class BPlusTree(object):
+    def __init__(self, order=8):
+        self.root = Node(order)
+
+    def find(self, node, key):
+        for i, item in enumerate(node.keys):
+            if key < item:
+                return node.values[i], i
+
+        return node.values[i + 1], i + 1
+
+    def merge(self, parent, child, index):
+        parent.values.pop(index)
+        pivot = child.keys[0]
+
+        for i, item in enumerate(parent.keys):
+            if pivot < item:
+                parent.keys = parent.keys[:i] + [pivot] + parent.keys[i:]
+                parent.values = parent.values[:i] + child.values + parent.values[i:]
+                break
+
+           elif i + 1 == len(parent.keys):
+                parent.keys += [pivot]
+                parent.values += child.values
+                break
+
+    def insert(self, key, value):
+        parent = None
+        child = self.root
+
+        while not child.leaf:
+            parent = child
+            child, index = self.find(child, key)
+
+        child.add(key, value)
+
+        if child.is_full():
+            child.split()
+
+            if parent and not parent.is_full():
+                self.merge(parent, child, index)
+
+    def show(self):
+        self.root.show()
